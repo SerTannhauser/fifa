ae3010fa0c2a871bdc65ff207b5140d3151f7363
SerTannhauser <43615195+SerTannhauser@users.noreply.github.com>
2018-10-15 10:41:47 -0400

    Final?

--- server.R
+++ server.R
@@ -22,12 +22,28 @@ library(ggthemes)
 library(reshape2)
 library(gridExtra)
 library(rworldmap)
+library(lubridate)
 
 
 function(input, output, session) {
   output$res <- renderText({
     req(input$sidebarItemExpanded)
     paste("Expanded menuItem:", input$sidebarItemExpanded)
+  })
+  
+  #### Dashboard
+  
+  output$menu <- renderMenu({
+    sidebarMenu(
+      id = "tabs",
+      menuItem("Overview", tabName = "overview", icon = icon("info")),
+      menuItem("Fifa Rankings", tabName = "fifa_rankings", icon = icon("globe")),
+      menuItem("Confederations", tabName = "confederations", icon = icon("cloud")),
+      menuItem("Confederation Comparison", tabName = "confed_comparison", icon = icon("bars")),
+      menuItem("Historical Rankings", tabName = "nation_rankings", icon = icon("clone")),
+      menuItem("Average Rankings", tabName = "average_rankings", icon = icon("align-justify")),
+      menuItem("Findings", tabName = "findings", icon = icon("cog"))
+    )
   })
   
   #### FIFA Histogram
@@ -42,10 +58,11 @@ function(input, output, session) {
       ggplot(aes(x = reorder(country_full, -rank), y = total_points, fill = confederation)) +
       geom_bar(stat = 'identity') + coord_flip() + theme_economist(10) + 
       scale_fill_brewer(name = '', palette = 'Paired') + 
-      theme(legend.position = 'None', panel.grid.major.y = element_blank())
+      theme(legend.position = 'None', panel.grid.major.y = element_blank()) +
+      xlab("Country Name") + ylab("Points")
   })
   
-  #### Histogram
+  #### Confederation Histogram
   
   selectInput <- reactive ({
     switch(input$confed,
@@ -56,13 +73,85 @@ function(input, output, session) {
            "OFC" = OFC,
            "UEFA" = UEFA)
   })
+  
   output$confed_histo <- renderPlot ({
     fifa_confed <- data.frame(fifa_18 %>%
-                            dplyr::filter(confederation == input$confed))
+                                dplyr::filter(confederation == input$confed))
     
     ggplot(fifa_confed, aes(x = reorder(country_full, -rank), y = total_points, fill = confederation)) +
       geom_bar(stat = 'identity') + coord_flip() + theme_economist(10) +
-      scale_fill_brewer(name = '', palette = 'Paired') +
-      theme(legend.position= 'None', panel.grid.major.y = element_blank())
+      scale_fill_discrete() +
+      theme(legend.position= 'None', panel.grid.major.y = element_blank()) +
+      xlab("Country Name") + ylab("Total Points")
+  })
+  
+  #### Confederation Boxplot
+  
+  output$point_boxplot <- renderPlot ({
+    fifa_18 %>%
+      ggplot(aes(x = reorder(confederation, total_points, FUN = mean), y = total_points, fill = confederation)) +
+      geom_boxplot(alpha=.75,size=.25) +
+      geom_jitter(shape = 16, position = position_jitter(0.25), size = 1, alpha = .5) +
+      theme_fivethirtyeight() + theme(legend.position = 'None') +
+      scale_fill_discrete() +
+      coord_flip()
+  })
+  
+  output$ranking_boxplot <- renderPlot ({
+    fifa_18 %>%
+      ggplot(aes(x = reorder(confederation, -rank, FUN = mean), y = -rank, fill = confederation)) +
+      geom_boxplot(alpha=.75,size=.25) +
+      geom_jitter(shape = 16, position = position_jitter(0.25), size = 1, alpha = .5) +
+      theme_fivethirtyeight() + theme(legend.position = 'None') +
+      scale_fill_discrete() +
+      coord_flip()
+  })
+  
+  #### Nation Rankings by Confederation Histogram
+  
+  selectInput <- reactive ({
+    switch(input$nation_confed,
+           "AFC" = AFC,
+           "CAF" = CAF,
+           "CONCACAF" = CONCACAF,
+           "CONMEBOL" = CONMEBOL,
+           "OFC" = OFC,
+           "UEFA" = UEFA)
+  })
+  
+  output$nations_histo <- renderPlot ({
+    
+    nation_confed <- data.frame(fifa %>%
+                                  dplyr::filter(confederation == input$nation_confed) %>%
+                                  dplyr::mutate(rank_date = as.Date(rank_date, format = "%m/%d/%Y"), "%Y") %>%
+                                  dplyr::group_by(rank, rank_date, country_full) %>%
+                                  dplyr::summarise(meanRank = mean(rank)))
+    
+    nation_confed %>%
+      ggplot(aes(x = rank_date, y = meanRank, color = meanRank)) + 
+      geom_line(aes(group = 1), alpha = 1, size = 1.) + 
+      theme_fivethirtyeight(10) +
+      scale_y_reverse() + ylim(220, 0) +
+      facet_wrap(~ country_full, ncol = 4)
+  })
+  
+  #### Average Rankings Boxplots
+  
+  output$nations_rank <- renderPlot ({
+    nation_rank <- data.frame(fifa %>%
+                                dplyr::mutate(rank_date = as.Date(rank_date, format = "%m/%d/%Y"), "%Y") %>%
+                                dplyr::mutate(country_full = as.character(country_full)) %>% 
+                                dplyr::group_by(rank, rank_date, country_abrv, country_full) %>%
+                                dplyr::summarise(meanRank = mean(rank)) %>%
+                                dplyr::filter(meanRank < input$rank_max))
+    
+    nation_rank %>%
+      ggplot(aes(x = reorder(country_full, -rank), y = -meanRank, fill = country_full)) +
+      geom_boxplot(alpha = .75, size = .5) +
+      geom_jitter(shape = 16, position = position_jitter(0.2), size = 1, alpha = .5) +
+      theme_economist(8) + theme(legend.position = 'None') +
+      scale_fill_discrete() +
+      coord_flip() + 
+      xlab("Country") + ylab("Rank")
   })
 }
--- ui.R
+++ ui.R
@@ -15,41 +15,94 @@ dashboardPage(
   
   #### Sidebar
   dashboardSidebar(
-    sidebarMenu(
-      menuItem("Home", tabName = "home", icon = icon("dashboard")),
-      menuItem("Background", tabName = "background", icon = icon("futbol")),
-      menuItem("Confederations", tabName = "confederations", icon = icon("trophy")),
-      menuItem("Confederation Rankings", tabName = "confederation rankings", icon = icon("th"))
-               )
-      ),
+    sidebarMenuOutput("menu"),
     textOutput("res")
-    )
+    ),
   
   #### Dashboard Body
   dashboardBody(
     tabItems(
-      tabItem("home", img(src="https://upload.wikimedia.org/wikipedia/commons/a/aa/FIFA_logo_without_slogan.svg", 
-                                  width="100%")),
-      
-      tabItem("background", "History", style = "font-size:25px",
-              br(),
-              img(src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/World_Map_FIFA.svg/1200px-World_Map_FIFA.svg.png"),
-              br(),
-              p(paste("FIFA was founded in 1904 to oversee international competitions by the national associations of Belgium, Denmark, France, Germany, the Netherlands, Spain, Sweden, and Switzerland.  Headquartered in Zurich, Switzerland, FIFA now consists of 211 national associations divided into 6 regional confederations.  While not in control over the rules of the game of football (soccer), FIFA is responsible for the organization and promotion of a number of tournament - most important of which is the World Cup."))),
-      tabItem("confederation rankings",
-              inputPanel(
-                selectInput("confed", "Confederation:", choices = c('AFC','CAF','CONCACAF','CONMEBOL','OFC','UEFA')),
-                hr(),
-                helpText("Select Confederation")),
-              mainPanel(
-                plotOutput("confed_histo", height = 700))),
-      tabItem("confederation rankings",
-              inputPanel(
-                selectInput("confed", "Confederation:", choices = c('AFC','CAF','CONCACAF','CONMEBOL','OFC','UEFA')),
-                hr(),
-                helpText("Select Confederation")),
-              mainPanel(
-                plotOutput("confed_histo", height = 700)))
-      )
+      tabItem(tabName = "overview", titlePanel("Overview"),
+              box(title = "Background", height = 525,
+                  img(src="http://img.photobucket.com/albums/v669/5CN/FIFA%20logo_zps39tja4ga.jpg?t=1539447072", width = "95%"),
+                  br(),
+                  p(paste("FIFA was founded in 1904 to oversee international competitions by the national associations of Belgium, Denmark, France, Germany, the Netherlands, Spain, Sweden, and Switzerland.  Headquartered in Zurich, Switzerland, FIFA considers itself to be the international governing body for association football, futsal, and beach soccer.  While not in control over the rules of the game of football (soccer), FIFA is responsible for the organization and promotion of a number of tournament - most important of which is the World Cup."))),
+              box(title = "Confederations", height = 525,
+                  img(src="http://img.photobucket.com/albums/v669/5CN/fifa_zpswzdlqdpg.png?t=1539447089", width = "95%"),
+                  br(),
+                  p(paste("Today, FIFA is composed of 211 member nations divided into six geographic confederations that roughly correspond to the six populated continents:"),
+                    tags$ul(
+                      tags$li("AFC - Asia Football Confederation"),
+                      tags$li("CAF - Confederation Africaine de Football"),
+                      tags$li("CONCACAF - Confederation of North, Central American and Caribbean Association Football"),
+                      tags$li("CONMEBOL - Confederacion Sudamericana de Futbol"),
+                      tags$li("OFC - Oceania Footbal Confederation"),
+                      tags$li("UEFA - Union of European Football Associations"))
+                    ))),
+      tabItem(tabName = "fifa_rankings", titlePanel("FIFA Rankings"), 
+              plotOutput(outputId = "fifa_histo", height = 2000)),
+      tabItem(tabName = "confederations", titlePanel("Rankings by Confederation - June 7, 2018"),
+              sidebarLayout(
+                sidebarPanel(
+                  selectInput("confed", "Confederation:", choices = c('AFC','CAF','CONCACAF','CONMEBOL','OFC','UEFA')),
+                  hr(),
+                  helpText("Select Confederation")),
+                mainPanel(
+                  plotOutput("confed_histo", height = 800)),
+                position = "right")),
+      tabItem(tabName = "confed_comparison", titlePanel("Confederation Comparisons"),
+              tabsetPanel(type = "tabs",
+                          tabPanel("Points",
+                                   plotOutput(outputId= "point_boxplot")),
+                          tabPanel("Ranking",
+                                   plotOutput(outputId= "ranking_boxplot")))),
+      tabItem(tabName = "nation_rankings", titlePanel("Historical Rankings by Confederation"),
+              fluidRow(
+                box(
+                  selectInput("nation_confed", "Historical Rankings (1993-2018):", choices = c('AFC','CAF','CONCACAF','CONMEBOL','OFC','UEFA')),
+                  hr(),
+                  helpText("Select Confederation"))),
+              fluidRow(
+                box(width = "100%",
+                    plotOutput("nations_histo", height = 1000)))),
+      tabItem(tabName = "average_rankings", titlePanel("Average Ranking of Nations"),
+              fluidRow(
+                box(
+                  sliderInput("rank_max", "Average Rankings Range:", min = 1, max = 211, value = c(10)),
+                  hr(),
+                  helpText("Select Maximum Rank for Range"))),
+              fluidRow(
+                box(width = "100%",
+                  plotOutput("nations_rank", height = 1200)))),
+      tabItem(tabName = "findings",
+              tabsetPanel(
+                tabPanel(title = "Research Questions",
+                         br(),
+                         p(tags$ol(
+                           tags$li("How do the confederations rank when considering their member nations?"),
+                           tags$li("Which nations have performed consistently well since 1993?"),
+                           tags$li("Which nations within each confederation have performed consistently well?"),
+                           tags$li("Which nations have improved (or worsened) since 1993?")))),
+                tabPanel(title = "Findings",
+                         br(),
+                         p(tags$ul(
+                           tags$li("While the highest ranked nation is from Europe (Germany), South America ranks higher on average."),
+                           tags$li("UEFA's average ranking and points is heavily skewed by a number of smaller, outlier nations."),
+                           tags$li("CONCACAF is bottom heavy because of the smaller nations in the Caribbean and Central America."),
+                           tags$li("OFC is by far the weakest confederation."),
+                           tags$li("Nations that have ranked #1 include Argentina, Belgium, Brazil, France,Germany, the Netherlands, Italy, and Spain.  Of these nations, only Belgium (won third placed) and the Netherlands (reached two finals) have failed to win the World Cup." ),
+                           tags$li("Outside of the traditional European and South America footballing powers, most of the confederations have been dominated by a handful of countries:",
+                                   tags$li("Asia has been dominated by Australia, Iran, Japan, and South Korea."),
+                                   tags$li("Africa has been dominated by North Africa (Algeria, Egypt, Tunisia) and West Africa (Cameroon, Ghana, Ivory Coast, Nigeria, Senegal)."),
+                                   tags$li("CONCACAF has been dominated by Mexico and the United States."),
+                                   tags$li("New Zealand is the only Oceania country to have done well.")),
+                           tags$li("The smaller nations of the world, especially the island nations perform abysmally."),
+                           tags$li("Kuwait, Singapore and Thailand have dropped percipituously."),
+                           tags$li("Cape Verde is on the rise, while Angola, Zambia, and Zimbabwe have fallen."),
+                           tags$li("Costa Rica has risen, overtaking the United States as number two in CONCACAF after the American failure to qualify for the 2018 World Cup."),
+                           tags$li("Peru and Chile are on the rise in South America."),
+                           tags$li("The nations that formerly made up Yugoslavia have all done well as independent teams."),
+                           tags$li("Cuba and New Zealand have had the most volatile rise and fall in rankings."))))))
     )
   )
+)
